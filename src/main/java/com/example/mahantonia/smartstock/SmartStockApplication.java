b6251c0d5efa78c8bc8398bbcacb55c8883089c9
Maria Antonia <maanfecon@hotmail.com>
2021-03-23 20:41:02 -0300

    Create project

---
+++ src/main/java/com/example/mahantonia/smartstock/SmartStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.mahantonia.smartstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mahantonia/smartstock/SmartStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mahantonia.smartstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
